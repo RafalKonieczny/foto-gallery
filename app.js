c348c8cb3bda209dbf4b3ad7539b01ce537d01ba
Rafał Konieczny <rkonieczny87@gmail.com>
2022-11-06 23:54:34 +0100

    added arrows and expanded gallery navigation

--- app.js
+++ app.js
@@ -1,18 +1,84 @@
 const THUMBNAILS = document.querySelectorAll(".thumbnail img");
 const POPUP = document.querySelector(".popup");
-const POPUP__ClOSE = document.querySelector(".popup__close");
-const POPUP__IMG = document.querySelector(".popup__img");
+const POPUP_CLOSE = document.querySelector(".popup__close");
+const POPUP_IMG = document.querySelector(".popup__img");
+const ARROW_LEFT = document.querySelector(".popup__arrow--left");
+const ARROW_RIGHT = document.querySelector(".popup__arrow--right");
 
+let currentImgIndex;
 
-THUMBNAILS.forEach((thumbnail) => {
-    thumbnail.addEventListener("click", (e) => {
+const showNextImg = () => {
+    if (currentImgIndex === THUMBNAILS.length - 1) {
+        currentImgIndex =  0;
+    } else {
+        currentImgIndex++;
+    }
+    POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
+}
+
+const showPreviousImg =() => {
+    if (currentImgIndex === 0) {
+        currentImgIndex =  THUMBNAILS.length - 1;
+    } else {
+        currentImgIndex--;
+    }
+    POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
+}
+
+const closePopup = () => {
+    POPUP.classList.add("fade-out");
+    setTimeout(() => {
+        POPUP.classList.add("hidden");
+        POPUP.classList.remove("fade-out");
+        THUMBNAILS.forEach(element => {
+            element.setAttribute("tabindex", 1);
+        })
+    }, 300 ); 
+}
+
+THUMBNAILS.forEach((thumbnail, index) => {
+    const showPopup = (e) => {
         POPUP.classList.remove("hidden");
-        POPUP__IMG.src= e.target.src;
-    })
+        POPUP_IMG.src = e.target.src;
+        currentImgIndex = index;
+        THUMBNAILS.forEach(element => {
+            element.setAttribute("tabindex", -1);
+        });
+    };
+
+    thumbnail.addEventListener("click", showPopup); 
+
+    thumbnail.addEventListener("keydown", (e) => {
+        if (e.code === "Enter") {
+            showPopup(e);
+        }
+    });
 });
 
-POPUP__ClOSE.addEventListener("click", () => {
-    POPUP.classList.add("hidden");
+POPUP_CLOSE.addEventListener("click", closePopup);
+
+ARROW_RIGHT.addEventListener("click", showNextImg);
+
+ARROW_LEFT.addEventListener("click", showPreviousImg);
+
+document.addEventListener("keydown", (e) => {
+    if (!POPUP.classList.contains("hidden")) {
+        if (e.code === "ArrowRight" ) {
+            showNextImg();
+        }
+
+        if (e.code ==="ArrowLeft") {
+            showPreviousImg();
+        }
+
+        if (e.code === ("Escape")) {
+            closePopup();
+        }
+    }
 });
 
-
+POPUP.addEventListener("click", (e) => {
+    if(e.target === POPUP) {
+        closePopup();
+    }
+});
